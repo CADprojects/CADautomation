fdbdbbe3eeb116222a7cbd64b2c202f4c97622e8
CADprojects <member143@gmail.com>
2016-12-08 17:03:50 +0300

    exportTestBase modified

--- src/main/java/base/ExportTestBase.java
+++ src/main/java/base/ExportTestBase.java
@@ -32,34 +32,27 @@ public class ExportTestBase extends TestBase {
     @Override
     @BeforeMethod(alwaysRun = true)
     public void setUp(){
-//        String browser = System.getProperty("browser").toLowerCase();
-//        switch (browser) {
-//            case "chrome":
-//                Map<String, Object> prefs = new HashMap<>();
-//                prefs.put("download.default_directory", FOLDERFOREXPORT);
-//                ChromeOptions options = new ChromeOptions();
-//                options.setExperimentalOption("prefs", prefs);
-//                DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-//                capabilities.setCapability(ChromeOptions.CAPABILITY, options);
-//                driver = new ChromeDriver(capabilities);
-//                break;
-//            default:
-//                FirefoxProfile f = new FirefoxProfile();
-//                f.setPreference("browser.download.folderList", 2);
-//                f.setPreference("browser.download.dir", FOLDERFOREXPORT);
-//                f.setPreference("browser.helperApps.alwaysAsk.force", false);
-//                f.setPreference("browser.download.manager.showWhenStarting", false);
-//                f.setPreference("browser.helperApps.neverAsk.saveToDisk", "text/csv");
-//                driver = new FirefoxDriver(f);
-//                break;
-//        }
-        FirefoxProfile f = new FirefoxProfile();
-        f.setPreference("browser.download.folderList", 2);
-        f.setPreference("browser.download.dir", FOLDERFOREXPORT);
-        f.setPreference("browser.helperApps.alwaysAsk.force", false);
-        f.setPreference("browser.download.manager.showWhenStarting", false);
-        f.setPreference("browser.helperApps.neverAsk.saveToDisk", "text/csv");
-        driver = new FirefoxDriver(f);
+        String browser = System.getProperty("browser").toLowerCase();
+        switch (browser) {
+            case "chrome":
+                Map<String, Object> prefs = new HashMap<>();
+                prefs.put("download.default_directory", FOLDERFOREXPORT);
+                ChromeOptions options = new ChromeOptions();
+                options.setExperimentalOption("prefs", prefs);
+                DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+                capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+                driver = new ChromeDriver(capabilities);
+                break;
+            default:
+                FirefoxProfile f = new FirefoxProfile();
+                f.setPreference("browser.download.folderList", 2);
+                f.setPreference("browser.download.dir", FOLDERFOREXPORT);
+                f.setPreference("browser.helperApps.alwaysAsk.force", false);
+                f.setPreference("browser.download.manager.showWhenStarting", false);
+                f.setPreference("browser.helperApps.neverAsk.saveToDisk", "text/csv");
+                driver = new FirefoxDriver(f);
+                break;
+        }
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get(URL);
