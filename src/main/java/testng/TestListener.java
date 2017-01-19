b55487bf6db336ece5b0dfd88b2e3641a119844f
CADprojects <member143@gmail.com>
2017-01-19 14:30:26 +0300

    implemented logging to file functionality

--- src/main/java/testng/TestListener.java
+++ src/main/java/testng/TestListener.java
@@ -5,6 +5,8 @@ import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.testng.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
@@ -15,20 +17,25 @@ import java.io.IOException;
 public class TestListener implements ITestListener {
     @Override
     public void onTestStart(ITestResult iTestResult) {
+        getLogger().info(String.format("%s started", getMethodName(iTestResult)));
     }
 
     @Override
     public void onTestSuccess(ITestResult iTestResult) {
+        getLogger().info(String.format("%s finished successfully", getMethodName(iTestResult)));
     }
 
     @Override
     public void onTestFailure(ITestResult iTestResult) {
-        String methodName = iTestResult.getName();
+        String methodName = getMethodName(iTestResult);
         if(!iTestResult.isSuccess()){
+            Logger logger = getLogger();
+            logger.error(String.format("%s failed", methodName));
+            logger.error(iTestResult.getThrowable().toString());
             File screenshotFile = ((TakesScreenshot) PageBase.getDriver()).getScreenshotAs(OutputType.FILE);
             try {
                 String reportDirectory = new File(System.getProperty("user.dir")) + "\\target\\surefire-reports";
-                File resultFile = new File((String) reportDirectory+"\\failure_screenshots\\" + methodName +".png");
+                File resultFile = new File(reportDirectory+"\\failure_screenshots\\" + methodName +".png");
                 FileUtils.copyFile(screenshotFile, resultFile);
                 Reporter.log("<a href='"+ resultFile + "'> <img src='"+ resultFile + "' height='200' width='200'/> </a>");
             } catch (IOException e) {
@@ -39,6 +46,7 @@ public class TestListener implements ITestListener {
 
     @Override
     public void onTestSkipped(ITestResult iTestResult) {
+        getLogger().info(String.format("%s skipped", getMethodName(iTestResult)));
     }
 
     @Override
@@ -47,9 +55,23 @@ public class TestListener implements ITestListener {
 
     @Override
     public void onStart(ITestContext iTestContext) {
+        getLogger().info(String.format("Test suite %s started", getMethodName(iTestContext)));
     }
 
     @Override
     public void onFinish(ITestContext iTestContext) {
+        getLogger().info(String.format("Test suite %s finished", getMethodName(iTestContext)));
+    }
+
+    private Logger getLogger() {
+        return LoggerFactory.getLogger(TestListener.class);
+    }
+
+    private String getMethodName(ITestResult iTestResult) {
+        return iTestResult.getName();
+    }
+
+    private String getMethodName(ITestContext iTestContext) {
+        return iTestContext.getName();
     }
 }
